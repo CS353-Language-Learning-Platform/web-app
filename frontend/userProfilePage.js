acdf658619277184bcac7f386e59d12714540b1a
Mustafa <quelstriless@gmail.com>
2023-12-17 17:57:10 +0300

    frontend-base

---
+++ frontend/userProfilePage.js
@@ -0,0 +1,12 @@
+function myFunction() {
+    alert("Butona tıklandı!");
+  }
+  function showDiv(id) {
+    // Tüm div'leri gizle
+    document.getElementById('accountSettings').style.display = 'none';
+    document.getElementById('notifications').style.display = 'none';
+    document.getElementById('memberships').style.display = 'none';
+
+    // Seçilen div'i göster
+    document.getElementById(id).style.display = 'block';
+}

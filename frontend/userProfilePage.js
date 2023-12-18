613ecadeb8d5e3c670e41a49f0c72fed64361f13
Yusuf Dogan <yusuf.dogan@ug.bilkent.edu.tr>
2023-12-18 06:33:01 +0300

    Student Rate feature

--- frontend/userProfilePage.js
+++ frontend/userProfilePage.js
@@ -6,6 +6,8 @@ function myFunction() {
     document.getElementById('accountSettings').style.display = 'none';
     document.getElementById('notifications').style.display = 'none';
     document.getElementById('memberships').style.display = 'none';
+    document.getElementById('student-session-container').style.display = 'none';
+
 
     // Seçilen div'i göster
     document.getElementById(id).style.display = 'block';

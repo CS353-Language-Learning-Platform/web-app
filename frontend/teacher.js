ab7bbab58a146d9f6d86067e4e3cb8d2cb734ae3
Yusuf Dogan <yusuf.dogan@ug.bilkent.edu.tr>
2023-12-19 01:56:23 +0300

    Some updates

--- frontend/teacher.js
+++ frontend/teacher.js
@@ -5,6 +5,8 @@ function showDiv(id) {
   document.getElementById('notifications').style.display = 'none';
   document.getElementById('sources').style.display = 'none';
   document.getElementById('announcement-container').style.display = 'none';
+  document.getElementById('search-results').style.display = 'none';
+
 
   // Seçilen div'i göster
   document.getElementById(id).style.display = 'block';
@@ -51,4 +53,3 @@ window.onclick = function(event) {
     }
 }
 
-// Handle the feedback submission
